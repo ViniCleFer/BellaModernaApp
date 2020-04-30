f8c7d13731012f044a82292cb7624bb98787f8b9
unknown <viniciusclementefernandes@gmail.com>
2020-04-30 16:10:00 -0300

    Novas Navegações, Telas More e Account Ok

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -1,30 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   heightPercentageToDP as hp,
   widthPercentageToDP as wp,
 } from 'react-native-responsive-screen';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { useSelector } from 'react-redux';
 
 import {
   Container,
   TitleArea,
   Title,
-  PersonArea,
-  PersonImage,
   AreaInfo,
-  PersonName,
-  PersonEmail,
-  PerfilArea,
-  PersonPerfil,
-  PersonAreaInfo,
   NavButton,
   AreaButton,
   TextNavButton,
 } from './styles';
 
 export default function Account(props) {
-  const profile = useSelector((state) => state.user.profile);
   const navigateBack = () => {
     props.navigation.goBack();
   };
@@ -41,24 +32,6 @@ export default function Account(props) {
         />
         <Title>Mais Opções</Title>
       </TitleArea>
-      <PersonArea>
-        <PersonImage
-          style={{ heigh: 50, width: 50 }}
-          source={{
-            uri: `http://192.168.0.13:3333/files/${profile.image_url}`,
-          }}
-        />
-        <PersonAreaInfo>
-          <PersonName>{profile.name}</PersonName>
-          <PersonEmail>vinicius@gmail.com</PersonEmail>
-          <PerfilArea
-            underlayColor="#f2f1f3"
-            onPress={() => props.navigation.navigate('StackAccount')}
-          >
-            <PersonPerfil>Editar perfil</PersonPerfil>
-          </PerfilArea>
-        </PersonAreaInfo>
-      </PersonArea>
       <AreaInfo>
         <NavButton underlayColor="#f2f1f3" onPress={() => {}}>
           <AreaButton>
@@ -69,7 +42,7 @@ export default function Account(props) {
               style={{ marginLeft: wp('5.94%') }}
               onPress={navigateBack}
             />
-            <TextNavButton>Questionários</TextNavButton>
+            <TextNavButton>Dados Pessoais</TextNavButton>
           </AreaButton>
         </NavButton>
         <NavButton underlayColor="#f2f1f3" onPress={() => {}}>
@@ -81,7 +54,7 @@ export default function Account(props) {
               style={{ marginLeft: wp('5.94%') }}
               onPress={navigateBack}
             />
-            <TextNavButton>Programas</TextNavButton>
+            <TextNavButton>Detalhes do Plano</TextNavButton>
           </AreaButton>
         </NavButton>
         <NavButton underlayColor="#f2f1f3" onPress={() => {}}>
@@ -93,31 +66,7 @@ export default function Account(props) {
               style={{ marginLeft: wp('5.94%') }}
               onPress={navigateBack}
             />
-            <TextNavButton>Conta</TextNavButton>
-          </AreaButton>
-        </NavButton>
-        <NavButton underlayColor="#f2f1f3" onPress={() => {}}>
-          <AreaButton>
-            <Icon
-              name="information-outline"
-              size={(hp('3.87%'), wp('6.88%'))}
-              color="#625C70"
-              style={{ marginLeft: wp('5.94%') }}
-              onPress={navigateBack}
-            />
-            <TextNavButton>Informações Legais</TextNavButton>
-          </AreaButton>
-        </NavButton>
-        <NavButton underlayColor="#f2f1f3" onPress={() => {}}>
-          <AreaButton>
-            <Icon
-              name="logout-variant"
-              size={(hp('3.87%'), wp('6.88%'))}
-              color="#625C70"
-              style={{ marginLeft: wp('5.94%') }}
-              onPress={navigateBack}
-            />
-            <TextNavButton>Sair</TextNavButton>
+            <TextNavButton>Forma de Pagamento</TextNavButton>
           </AreaButton>
         </NavButton>
       </AreaInfo>
