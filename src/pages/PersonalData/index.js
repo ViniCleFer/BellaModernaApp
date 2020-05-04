9140e2ca2db4ab9fb94cdd4b08dc331a83468c5d
unknown <viniciusclementefernandes@gmail.com>
2020-05-04 11:57:11 -0300

    Tela PersonData finalizada

--- src/pages/PersonalData/index.js
+++ src/pages/PersonalData/index.js
@@ -1,8 +1,221 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useMemo } from 'react';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
+import { format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import {
+  Container,
+  TitleArea,
+  Title,
+  ProfileArea,
+  ProfilePhoto,
+  LinkPhoto,
+  LinkTextPhoto,
+  SubtitleArea,
+  Subtitle,
+  AreaInfo,
+  NavButton,
+  ButtonNavArea,
+  ButtonArea,
+  IconArea,
+  TextNavButton,
+  TextProfile,
+  NavButtonAddress,
+  AreaAddress,
+  TextProfileAddress,
+} from './styles';
 
-export default function PersonalData() {
-  return <Text>Dados pessoais</Text>;
+export default function PersonalData(props) {
+  const profile = useSelector((state) => state.user.profile);
+
+  const navigateBack = () => {
+    props.navigation.navigate('Account');
+  };
+
+  const dateFormatted = useMemo(
+    () =>
+      format(new Date(profile.birth), 'dd/MM/yyyy', {
+        locale: pt,
+      }),
+    [profile.birth]
+  );
+
+  return (
+    <Container>
+      <TitleArea>
+        <Icon
+          name="arrow-back"
+          size={(hp('2.82%'), wp('5%'))}
+          color="#625C70"
+          style={{ marginLeft: wp('7.5%') }}
+          onPress={navigateBack}
+        />
+        <Title>Dados Pessoais</Title>
+      </TitleArea>
+      <ProfileArea>
+        <ProfilePhoto
+          source={{
+            uri: `http://192.168.0.13:3333/files/${profile.image_url}`,
+          }}
+        />
+
+        <LinkPhoto onPress={() => {}}>
+          <LinkTextPhoto>Alterar foto</LinkTextPhoto>
+        </LinkPhoto>
+      </ProfileArea>
+      <SubtitleArea>
+        <Subtitle>Dados pessoais</Subtitle>
+      </SubtitleArea>
+      <AreaInfo>
+        <NavButton
+          underlayColor="#f2f1f3"
+          onPress={() => props.navigation.navigate('Name')}
+        >
+          <ButtonNavArea>
+            <ButtonArea>
+              <TextNavButton>Nome</TextNavButton>
+              <TextProfile>{profile.name}</TextProfile>
+            </ButtonArea>
+            <IconArea>
+              <Icon
+                name="keyboard-arrow-right"
+                size={20}
+                color="#958FA3"
+                style={{ marginRight: hp('3.35%') }}
+              />
+            </IconArea>
+          </ButtonNavArea>
+        </NavButton>
+        <NavButton
+          underlayColor="#f2f1f3"
+          onPress={() => props.navigation.navigate('Phone')}
+        >
+          <ButtonNavArea>
+            <ButtonArea>
+              <TextNavButton>Telefone</TextNavButton>
+              <TextProfile>(11) 97745-6642</TextProfile>
+            </ButtonArea>
+            <IconArea>
+              <Icon
+                name="keyboard-arrow-right"
+                size={20}
+                color="#958FA3"
+                style={{ marginRight: hp('3.35%') }}
+              />
+            </IconArea>
+          </ButtonNavArea>
+        </NavButton>
+        <NavButton
+          underlayColor="#f2f1f3"
+          onPress={() => props.navigation.navigate('Email')}
+        >
+          <ButtonNavArea>
+            <ButtonArea>
+              <TextNavButton>CPF</TextNavButton>
+              <TextProfile>386.483.948-30</TextProfile>
+            </ButtonArea>
+            <IconArea>
+              <Icon
+                name="keyboard-arrow-right"
+                size={20}
+                color="#958FA3"
+                style={{ marginRight: hp('3.35%') }}
+              />
+            </IconArea>
+          </ButtonNavArea>
+        </NavButton>
+        <NavButton
+          underlayColor="#f2f1f3"
+          onPress={() => props.navigation.navigate('SocialRecord')}
+        >
+          <ButtonNavArea>
+            <ButtonArea>
+              <TextNavButton>Data de Nascimento</TextNavButton>
+              <TextProfile>{dateFormatted}</TextProfile>
+            </ButtonArea>
+            <IconArea>
+              <Icon
+                name="keyboard-arrow-right"
+                size={20}
+                color="#958FA3"
+                style={{ marginRight: hp('3.35%') }}
+              />
+            </IconArea>
+          </ButtonNavArea>
+        </NavButton>
+        <NavButtonAddress
+          underlayColor="#f2f1f3"
+          onPress={() => props.navigation.navigate('Adderss')}
+        >
+          <ButtonNavArea>
+            <ButtonArea>
+              <TextNavButton>Endereço</TextNavButton>
+              <AreaAddress>
+                <TextProfileAddress>
+                  Av. Comandandante Videlmo Munhoz, 75 Anhangabaú, Jundiaí - SP,
+                  13208-050
+                </TextProfileAddress>
+              </AreaAddress>
+            </ButtonArea>
+            <IconArea>
+              <Icon
+                name="keyboard-arrow-right"
+                size={20}
+                color="#958FA3"
+                style={{ marginRight: hp('3.35%') }}
+              />
+            </IconArea>
+          </ButtonNavArea>
+        </NavButtonAddress>
+      </AreaInfo>
+      <SubtitleArea>
+        <Subtitle>Login</Subtitle>
+      </SubtitleArea>
+      <AreaInfo>
+        <NavButton
+          underlayColor="#f2f1f3"
+          onPress={() => props.navigation.navigate('Email')}
+        >
+          <ButtonNavArea>
+            <ButtonArea>
+              <TextNavButton>E-mail</TextNavButton>
+              <TextProfile>{profile.email}</TextProfile>
+            </ButtonArea>
+            <IconArea>
+              <Icon
+                name="keyboard-arrow-right"
+                size={20}
+                color="#958FA3"
+                style={{ marginRight: hp('3.35%') }}
+              />
+            </IconArea>
+          </ButtonNavArea>
+        </NavButton>
+        <NavButton
+          underlayColor="#f2f1f3"
+          onPress={() => props.navigation.navigate('Password')}
+        >
+          <ButtonNavArea>
+            <ButtonArea>
+              <TextNavButton>Alterar Senha</TextNavButton>
+              <TextProfile>*************</TextProfile>
+            </ButtonArea>
+            <IconArea>
+              <Icon
+                name="keyboard-arrow-right"
+                size={20}
+                color="#958FA3"
+                style={{ marginRight: hp('3.35%') }}
+              />
+            </IconArea>
+          </ButtonNavArea>
+        </NavButton>
+      </AreaInfo>
+    </Container>
+  );
 }
---
+++ src/pages/PersonalData/styles.js
@@ -0,0 +1,134 @@
+import styled from 'styled-components/native';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+
+export const Container = styled.ScrollView`
+  flex: 1;
+  background-color: #fff;
+`;
+
+export const TitleArea = styled.View`
+  flex-direction: row;
+  height: ${hp('9.86%')};
+  align-items: center;
+  background-color: #fff;
+  border-color: #f2f1f3;
+  border-width: ${wp('0.36%')};
+  border-style: solid;
+`;
+
+export const Title = styled.Text`
+  align-items: center;
+  justify-content: center;
+  font-size: ${hp('2.82%')};
+  color: #625c70;
+  font-weight: bold;
+  margin-left: ${wp('7.5%')};
+`;
+
+export const ProfileArea = styled.View`
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+export const ProfilePhoto = styled.Image`
+  background-color: #0ff;
+  height: ${hp('17.61%')};
+  width: ${wp('31.25%')};
+  border-radius: 60;
+  justify-content: center;
+  align-items: center;
+  margin-top: ${hp('2.82%')};
+`;
+
+export const LinkPhoto = styled.TouchableHighlight`
+  background-color: #fff;
+  height: ${hp('2.99%')};
+  justify-content: center;
+
+  background: transparent;
+  margin-top: ${hp('0.70%')};
+  margin-bottom: ${hp('0;70%')};
+`;
+
+export const LinkTextPhoto = styled.Text`
+  font-size: ${hp('2.11%')};
+  color: #a51c60;
+  font-weight: normal;
+  align-items: center;
+`;
+
+export const SubtitleArea = styled.View`
+  height: ${hp('8.98%')};
+`;
+
+export const Subtitle = styled.Text`
+  font-size: ${hp('2.11%')};
+  color: #625c70;
+  font-weight: bold;
+  margin-left: ${wp('7.5%')};
+  margin-top: ${hp('4.75%')};
+`;
+
+export const AreaInfo = styled.View`
+  background-color: #fff;
+`;
+
+export const NavButton = styled.TouchableHighlight`
+  background-color: #fff;
+  height: ${hp('8.98%')};
+  justify-content: center;
+`;
+
+export const ButtonNavArea = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const ButtonArea = styled.View`
+  justify-content: center;
+`;
+
+export const IconArea = styled.View`
+  align-items: center;
+  justify-content: center;
+`;
+
+export const TextNavButton = styled.Text`
+  font-size: ${hp('2.11%')};
+  color: #312e38;
+  font-weight: normal;
+  align-items: center;
+  margin-left: ${wp('7.50%')};
+`;
+
+export const TextProfile = styled.Text`
+  font-size: ${hp('2.11%')};
+  color: #958fa3;
+  font-weight: normal;
+  align-items: center;
+  margin-left: ${wp('7.50%')};
+`;
+
+export const NavButtonAddress = styled.TouchableHighlight`
+  justify-content: center;
+  background: #fff;
+  padding-top: ${hp('1.51%')};
+  padding-bottom: ${hp('1.58%')};
+`;
+
+export const AreaAddress = styled.View`
+  justify-content: center;
+  width: ${wp('79.50%')};
+`;
+
+export const TextProfileAddress = styled.Text`
+  font-size: ${hp('2.11%')};
+  color: #958fa3;
+  font-weight: normal;
+  align-items: center;
+  margin-left: ${wp('7.50%')};
+`;
